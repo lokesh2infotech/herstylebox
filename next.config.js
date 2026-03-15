08d935215aeae789b9c459dfcc64568e3de1abe0
Lokesh Makhija <lokeshmakhija@Lokeshs-MacBook-Air.local>
2026-03-15 22:02:09 +0530

    Allow Vercel Blob domain in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'k5ovcfkgi9ig5nfk.public.blob.vercel-storage.com'],
     unoptimized: true,
   },
 }
